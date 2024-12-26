b7666512abd37fd75f6b32a5492e24b5b6a44e33
hsc996 <han.scaife@gmail.com>
2024-12-26 12:51:19 +1100

    added hyperlink to book container

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -5,18 +5,21 @@ export function BookList({ books }) {
     return (
         <>
             <div id="books-container">
-                {books.map((book) => {
+            {books.map((book) => {
+                    const bookLink = book.volumeInfo.canonicalVolumeLink || `https://books.google.com/books?id=${book.id}`;
                     return (
                         <div className="book-items" key={book.id}>
-                            {book.volumeInfo.imageLinks && (
-                                <img
-                                    className='book'
-                                    src={book.volumeInfo.imageLinks.smallThumbnail}
-                                    alt={book.volumeInfo.title || "Book thumbnail"}
-                                />
-                            )}
-                            <h4 className="name">{book.volumeInfo.title}</h4>
-                            <h4 className="author">By {book.volumeInfo.authors}</h4>
+                            <a href={bookLink} target="_blank" rel="noopener noreferrer">
+                                {book.volumeInfo.imageLinks && (
+                                    <img
+                                        className='book'
+                                        src={book.volumeInfo.imageLinks.smallThumbnail}
+                                        alt={book.volumeInfo.title || "Book thumbnail"}
+                                    />
+                                )}
+                                <h4 className="name">{book.volumeInfo.title}</h4>
+                                <h4 className="author">By {book.volumeInfo.authors}</h4>
+                            </a>
                         </div>
                     );
                 })}
