721308ea78b737b1864e6de474d3135ffbc7f6d6
hsc996 <han.scaife@gmail.com>
2025-01-20 10:36:06 +1100

    feat: search bar styling implemented

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,9 +3,9 @@ import NavBar from "./NavBar";
 
 export default function Layout({children}){
     return (
-        <div className="flex flex-col min-h-svh mx-auto -bg-background text-foreground" id="layout">
+        <div className="content-container" id="layout">
             <NavBar />
-            <main className="flex-grow p-4 max-w-80ch" id="main-content">
+            <main className="main-content" id="main-content">
                 {children}
             </main>
         </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
+import '../styles/SearchBar.css'
 
 
 export function SearchBar({ onSearch }){{
